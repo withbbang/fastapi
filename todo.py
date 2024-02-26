7a09720b2de2f1c82f46a6316dfbeb466be45fbb
김영선 <withbbang@gmail.com>
2024-02-26 19:03:43 +0900

    Test: 경로 파라미터 테스트

--- todo.py
+++ todo.py
@@ -15,3 +15,12 @@ async def say_hello(todo: Todo) -> dict:
 @todo_router.get("/todo")
 async def retrieve_todos() -> dict:
     return {"todos": todo_list}
+
+
+@todo_router.get("/todo/{id}")
+async def get_single_todo(id: int) -> dict:
+    for todo in todo_list:
+        if todo.id == id:
+            return {"todo": todo}
+
+    return {"message": "Todo with supplied ID doesn't exist"}
