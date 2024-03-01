19338ba102dfca915007e7013281cd37fdd07d8e
김영선 <withbbang@gmail.com>
2024-03-01 13:20:37 +0900

    Test: 환경변수 라우터 테스트

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from routes.todo import todo_router
 from routes.users import user_router
 from routes.events import event_router
+from routes.test import test_router
 
 app = FastAPI()
 
@@ -12,6 +13,7 @@ async def welcome() -> dict:
     return {"message": "Hello World"}
 
 
+app.include_router(test_router, prefix="/test")
 app.include_router(todo_router)
 app.include_router(user_router, prefix="/user")
 app.include_router(event_router, prefix="/event")
