938f2965ce1e5c8efaa99ee60f97c78e90ba54a8
김영선 <withbbang@gmail.com>
2024-03-18 23:03:32 +0900

    Docs: oracle db 제거

--- database/connection.py
+++ database/connection.py
@@ -1,5 +1,4 @@
 import os
-import oracledb
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,14 +9,3 @@ DSN = os.getenv("dsn")
 CONFIG_DIR = os.getenv("config_dir")
 WALLET_LOCATION = os.getenv("wallet_location")
 WALLET_PASSWORD = os.getenv("wallet_password")
-
-connection = oracledb.connect(
-    user=USERNAME,
-    password=PASSWORD,
-    dsn=DSN,
-    config_dir=CONFIG_DIR,
-    wallet_location=WALLET_LOCATION,
-    wallet_password=WALLET_PASSWORD,
-)
-
-cursor = connection.cursor()
--- index.py
+++ index.py
@@ -3,7 +3,6 @@ from fastapi import FastAPI
 from routes.todo import todo_router
 from routes.users import user_router
 from routes.events import event_router
-from routes.test import test_router
 
 app = FastAPI()
 
@@ -13,7 +12,6 @@ async def welcome() -> dict:
     return {"message": "Hello World"}
 
 
-app.include_router(test_router, prefix="/test")
 app.include_router(todo_router)
 app.include_router(user_router, prefix="/user")
 app.include_router(event_router, prefix="/event")
