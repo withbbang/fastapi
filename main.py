512e2000fd4512b78d86424687bfe809fba50933
김영선 <withbbang@gmail.com>
2024-04-12 17:58:00 +0900

    Fix: DB 동기화 에러 수정 중

--- main.py
+++ main.py
@@ -18,5 +18,5 @@ async def welcome() -> dict:
 app.include_router(member_router, prefix="/member")
 app.include_router(test_router, prefix="/test")
 
-if __name__ == "__index__":
+if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
